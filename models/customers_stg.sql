977ce50a6a1cb0890f4887fdfe9af595915222c2
Ratan sai <mandelaratansai@gmail.com>
2026-04-16 16:49:50 -0500

    yaml file to rename source tables

--- models/customers_stg.sql
+++ models/customers_stg.sql
@@ -2,4 +2,4 @@ select
     customerid, firstname, lastname, email, phone, address, city, state, zipcode, updated_at, 
     concat(firstname, ' ', lastname) as customer_name
 from
-    l1_landing.customers
+    {{  source('landing', 'cust')   }}
