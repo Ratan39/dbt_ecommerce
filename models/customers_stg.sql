d6cb0b538490117d6faf6823b35c93bfaee9c5ef
Ratan sai <mandelaratansai@gmail.com>
2026-04-28 11:50:05 -0500

    incremetal updates

--- models/customers_stg.sql
+++ models/customers_stg.sql
@@ -1,5 +1,7 @@
+{{ config(materialized='ephemeral') }}
+
 select 
     customerid, firstname, lastname, email, phone, address, city, state, zipcode, updated_at, 
     concat(firstname, ' ', lastname) as customer_name
 from
-    {{  source('landing', 'cust')   }}
+    {{  source('landing', 'customers')   }}
--- models/orders_stg.sql
+++ models/orders_stg.sql
@@ -1,3 +1,6 @@
+{{ config(materialized='table') }}
+
+
 select
     orderid, orderdate, customerid, employeeid, storeid, status as statuscd,
     case 
@@ -5,6 +8,14 @@ select
         when Status = '02' then 'Completed'
         when Status = '03' then 'Cancelled'
         Else Null
-    end as statusdesc, updated_at
+    end as statusdesc, 
+    case 
+        when storeid = 1000 then 'Online'
+        else 'In-store'
+    end as Order_channel, updated_at, current_timestamp as dbt_updated_at
 from 
-    l1_landing.orders
+    {{   source('landing', 'orders')   }}
+
+{% if is_incremental() %}
+where updated_at >= (select max(dbt_updated_at) from {{ this }})
+{% endif %}
